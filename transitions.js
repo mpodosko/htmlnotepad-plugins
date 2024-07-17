f8a20888e763634c69e431676fd536c96c82b7d9
MPodosko <mpodosko@gmail.com>
2024-07-17 18:07:42 +0300

    Update transitions.js

--- transitions.js
+++ transitions.js
@@ -1,1 +1,18 @@
-//
+if(Number(versionString.slice(0,3)) > 1.5) {
+  pluginnames.push('Transitions');  
+}
+document.body.innerHTML += '<style id="transitions_style"></style>';
+settings_main.innerHTML += '<input type="checkbox" id="transitions_checkbox"><label for="transitions_checkbox">Анимации</label>'
+transitions_checkbox.oninput = () => {
+  if(transions_checkbox.checked) {
+    localStorage.setItem('transitions', 'true');
+    transitions_style.innerHTML = '*{transition:all 0.25s !important;}';
+  } else {
+    localStorage.setItem('transitions', 'false');
+    transitions_style.innerHTML = '';
+  }
+}
+
+if(localStorage.getItem('transitions') == 'true') {
+  transitions_style.innerHTML = '*{transition:all 0.25s !important;}';
+}
