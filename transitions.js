8b3a134084ca60ab858b46e3fe59122c9a442c4c
MPodosko <mpodosko@gmail.com>
2024-07-17 22:29:00 +0300

    Update transitions.js

--- transitions.js
+++ transitions.js
@@ -4,7 +4,7 @@ try{if(Number(versionString.slice(0,3)) > 1.5) {
 document.body.insertAdjacentHTML('beforeend','<style id="transitions_style"></style>');
 settings_main.insertAdjacentHTML('beforeend','<input type="checkbox" id="transitions_checkbox"><label for="transitions_checkbox">Анимации</label>');
 transitions_checkbox.oninput = () => {
-  if(transions_checkbox.checked) {
+  if(transitions_checkbox.checked) {
     localStorage.setItem('transitions', 'true');
     transitions_style.innerHTML = '*{transition:all 0.25s !important;}';
   } else {
