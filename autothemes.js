43b50b04858458b28a6130f180af32aea210cd30
MPodosko <mpodosko@gmail.com>
2024-07-18 12:03:41 +0300

    Update autothemes.js

--- autothemes.js
+++ autothemes.js
@@ -6,5 +6,11 @@ settings_main.insertAdjacentHTML('beforeend',`
 <input type="checkbox" id="atc_checkbox"><label for="atc_checkbox">Включено</label>
 Выберите тему 1, нажмите "Сохранить тему 1", выберите тему 2, нажмите "Сохранить тему 2" и выберите период времени, когда будет активна тема 2
 <button>Сохранить тему 1</button><button>Сохранить тему 2</button>
-
+Начало: <input type="time" style="width:100px" id="atc_night_from">
+Конец:  <input type="time" style="width:100px" id="atc_night_to">
 `);
+
+function atc_interval() {
+  let d = new Date();
+  let timeString = `${(d.getHours() < 10 ? '0' : '') + d.getHours()}:${(d.getMinutes() < 10 ? '0' : '') + d.getMinutes()}`;
+}
